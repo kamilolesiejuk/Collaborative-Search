0c1c7e4854399d13f8329929c5366669e677182e
Kamil Olesiejuk <olesiejuk.kamil@gmail.com>
2011-04-05 15:47:57 +0200

    added CintextInitializer servlet for Objectify context setup

---
+++ src/collabsearch/ContextInitializer.java
@@ -0,0 +1,15 @@
+package collabsearch;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import com.googlecode.objectify.ObjectifyService;
+
+public class ContextInitializer implements ServletContextListener {
+
+ public void contextDestroyed(ServletContextEvent arg) {}
+
+ public void contextInitialized(ServletContextEvent arg) {
+  ObjectifyService.register(Document.class);
+  ObjectifyService.register(SearchUser.class);
+ }
+}
