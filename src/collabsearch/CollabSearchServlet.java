a1f031d3af122582b6bcadb083be9a949989ddb2
jbordoe <jesse.bordoe@gmail.com>
2011-04-05 07:38:10 -0700

    Servlet now sends simple messages upon requests from extension

--- src/collabsearch/CollabSearchServlet.java
+++ src/collabsearch/CollabSearchServlet.java
@@ -2,6 +2,8 @@ package collabsearch;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import org.codehaus.jackson.map.*;
+import java.util.Map;
 
 /**
  * 
@@ -14,8 +16,24 @@ public class CollabSearchServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
-	
-	
+		String reqType = req.getParameter("type");
+		//Retrieve and return results for given query
+		if (reqType.equals("query")){
+			String q = req.getParameter("q");
+			resp.getWriter().println("{\"status\":\"1\"," +
+					"\"pages\":[\"http://www.google.com\",\"http://www.tryHaskell.com\",\"http://www.bing.com\"]}");
+		} else if(reqType.equals("session")){
+			ObjectMapper mapper = new ObjectMapper();
+			try {				
+				Map<String,Object> session = mapper.readValue(req.getParameter("data"),Map.class);
+				String query = (String)session.get("query");
+				resp.getWriter().println("Data for '"+query+"' session recieved!");
+			} catch(Exception e) {
+				resp.getWriter().println("Error reading session data");
+			}
+			
+		} else {
+			resp.getWriter().println("Error sending session data");
+		}
+	}	
 }
