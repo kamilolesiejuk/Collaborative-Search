a95ab95a37c08bf344c7d988e291b48cd19ae051
Kamil Olesiejuk <olesiejuk.kamil@gmail.com>
2011-04-06 13:38:11 +0200

    added a VERY basic login page under /login

---
+++ src/collabsearch/LoginServlet.java
@@ -0,0 +1,34 @@
+package collabsearch;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
+public class LoginServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
+		UserService userService = UserServiceFactory.getUserService();
+
+		String thisURL = request.getRequestURI();
+		if (request.getUserPrincipal() != null) {
+			response.getWriter().println(
+					"<p>Hello, " + request.getUserPrincipal().getName()
+							+ "!  You can <a href=\""
+							+ userService.createLogoutURL(thisURL)
+							+ "\">sign out</a>.</p>");
+		} else {
+			response.getWriter().println(
+					"<p>Please <a href=\""
+							+ userService.createLoginURL(thisURL)
+							+ "\">sign in</a>.</p>");
+		}
+	}
+}
